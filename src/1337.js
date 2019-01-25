2b07778984e6bcaf5c5e4a5180813c10bc07a4e6
Rishav Rungta <rishav394@gmail.com>
2019-01-25 11:48:12 +0530

    Version bump
    Fix formatting, version bump, update gitignore
    Ready for browser_action

--- src/1337.js
+++ src/1337.js
@@ -1,94 +1,91 @@
-var old = ''
+var old = '';
+var warned = false;
 
-function sortTable(pos){
-  var rows = $('#mytable tbody tr').get();
-	rows.sort(function(a, b) {
+function sortTable(pos) {
+    var rows = $('#mytable tbody tr').get();
+    rows.sort(function (a, b) {
 
-  var A = Number($(a).children('td').eq(pos).text());
-  var B = Number($(b).children('td').eq(pos).text());
-  
-  if(A < B) {
-    return 1;
-  }
+        var A = Number($(a).children('td').eq(pos).text());
+        var B = Number($(b).children('td').eq(pos).text());
 
-  if(A > B) {
-    return -1;
-  }
+        if (A < B) {
+            return 1;
+        }
+        if (A > B) {
+            return -1;
+        }
+        return 0;
+    });
 
-  return 0;
-
-  });
-
-  $.each(rows, function(index, row) {
-    $('#mytable').children('tbody').append(row);
-  });
+    $.each(rows, function (_index, row) {
+        $('#mytable').children('tbody').append(row);
+    });
 }
 
 function main() {
-    console.log('ok')
-    
     var table = $('.table-list.table.table-responsive.table-striped');
-    table.attr('id','mytable')
-    
-    $('.coll-2').click(function(){
-        sortTable(1)
+    table.attr('id', 'mytable');
+
+    $('.coll-2').click(function () {
+        sortTable(1);
     });
-    $('.coll-3').click(function(){
-        sortTable(2)
+    $('.coll-3').click(function () {
+        sortTable(2);
     });
-    
-    
-    $(".js-modal-url").mouseenter(function(){
-        old = $(this).html()
-        var origin = $(this)
-        var url = $(this).attr('href')
-        if (url.search('jpeg.html')!== -1) {
-            url = url.replace('.html','').replace('a-i','').replace('b-i','').replace('ia-','')
-            var imageTag='<div style="position:relative;">'+'<img src="'+url+'" alt="'+url+'" height="300" />'+'</div>';
-            console.log(imageTag)
-            $(this).html(imageTag);
+
+
+    $(".js-modal-url").mouseenter(function () {
+        var origin = $(this);
+        old = origin.html();
+        var url = origin.attr('href');
+
+        // Handles URLs like https://22pixx.xyz/ia-o/2019/01/123456789.jpeg.html
+        if (url.search('jpeg.html') !== -1) {
+            url = url.replace('.html', '').replace('a-i', '').replace('b-i', '').replace('ia-', '');
+            var imageTag = '<div style="position:relative;">' + '<img src="' + url + '" alt="' + url + '" height="300" />' + '</div>';
+            console.log(imageTag);
+            origin.html(imageTag);
         }
+        // Handles pixsense.net text type links
         else if (origin.text().includes('pixsense.net')) {
-            url = url.replace('pixsense','imgfile')
-            // url = url.replace('https','http')
-        	console.log(url)
-        	$.ajax({
-			    url  : url,
-			    type : 'get',
-			    success : function(data, statusText, xhr){
-			        var status = Number(xhr.status)
-			        console.log('Status code: '+status)
-                    url = $(data).find('.big_img_box').children('img').attr('src')
-                    url = url.replace('http://','https://')
-		        	var imageTag='<div style="position:relative;">'+'<img src="'+url+'" alt="'+url+'" height="300" />'+'</div>';
-            		console.log(imageTag)
-            		console.log($(this))
-            		origin.html(imageTag);
-			    },
-			    error: function(jqXHR, exception){
-			    	var msg = '';
-			        if (jqXHR.status === 0) {
-			            msg = 'Please allow unsafe scripts to run';
-			        } else if (jqXHR.status == 404) {
-			            msg = 'Requested page not found. [404]';
-			        } else if (jqXHR.status == 500) {
-			            msg = 'Internal Server Error [500].';
-			        } else if (exception === 'parsererror') {
-			            msg = 'Requested JSON parse failed.';
-			        } else if (exception === 'timeout') {
-			            msg = 'Time out error.';
-			        } else if (exception === 'abort') {
-			            msg = 'Ajax request aborted.';
-			        } else {
-			            msg = 'Uncaught Error.\n' + jqXHR.responseText;
-			        }
-			        alert(msg)
-		        }
-		    });
+            url = url.replace('pixsense', 'imgfile');
+            $.ajax({
+                url: url,
+                type: 'get',
+                success: function (data, _statusText, xhr) {
+                    var status = Number(xhr.status);
+                    console.log('Status code: ' + status);
+                    url = $(data).find('.big_img_box').children('img').attr('src');
+                    url = url.replace('http://', 'https://');
+                    var imageTag = '<div style="position:relative;">' + '<img src="' + url + '" alt="' + url + '" height="300" />' + '</div>';
+                    console.log(imageTag);
+                    origin.html(imageTag);
+                },
+                error: function (jqXHR, exception) {
+                    var msg = '';
+                    if (jqXHR.status === 0) {
+                        msg = 'Please allow unsafe scripts to run by clicking the shield icon near the star in the address bar.';
+                    } else if (jqXHR.status == 404) {
+                        msg = 'Requested page not found. [404]';
+                    } else if (jqXHR.status == 500) {
+                        msg = 'Internal Server Error [500].';
+                    } else if (exception === 'timeout') {
+                        msg = 'Time out error.';
+                    } else if (exception === 'abort') {
+                        msg = 'Ajax request aborted.';
+                    } else {
+                        msg = 'Uncaught Error.\n' + jqXHR.responseText;
+                    }
+                    if (!warned) {
+                        alert(msg);
+                        warned = true;
+                    }
+                }
+            });
         }
     });
-    $(".js-modal-url").mouseleave(function(){
-        //$(this).html(old)
+    $(".js-modal-url").mouseleave(function () {
+        // $(this).html(old);
     });
 }
 
--- src/brazzers.js
+++ src/brazzers.js
@@ -1,20 +1,22 @@
-function main(){
+var user_choice = 'x1337x.ws';
+
+function main() {
     var j = $('.scene-title');
-    var url = 'https://x1337x.ws/search/'+j.text().replace('!','').replace('?','')+'/1/';
-    
+    var url = 'https://' + user_choice + '/search/' + j.text().replace('!', '').replace('?', '') + '/1/';
+
     $.ajax({
-    url  : url,
-    type : 'get',
-    success : function(data, statusText, xhr){
-        var status = Number(xhr.status)
-        console.log('Status code: '+status)
-        var res = data.match(new RegExp('<a href="/torrent/.+/">',''))
-        var url2 = 'https://x1337x.ws'+res[0].split('"')[1]
-        $('.video-data.clearfix ul').prepend('<li class="external-added"><a target="_blank" href="'+url2+'"><img src="https://cdn.iconscout.com/icon/premium/png-256-thumb/magnet-443-1049201.png" height=22 width=22/></a></li>');
-    },
-    error: function(){
-        alert('Looks like the torrent wesite didn\'t respond. Please try opening it manually once.')
-        window.open('https://x1337x.ws')
+        url: url,
+        type: 'get',
+        success: function (data, _statusText, xhr) {
+            var status = Number(xhr.status);
+            console.log('Status code: ' + status);
+            var res = data.match(new RegExp('<a href="/torrent/.+/">', ''));
+            var url2 = 'https://' + user_choice + '' + res[0].split('"')[1];
+            $('.video-data.clearfix ul').prepend('<li class="external-added"><a target="_blank" href="' + url2 + '"><img src="https://cdn.iconscout.com/icon/premium/png-256-thumb/magnet-443-1049201.png" height=22 width=22/></a></li>');
+        },
+        error: function () {
+            alert('Looks like the torrent wesite didn\'t respond. Please try opening it manually once.');
+            window.open('https://' + user_choice + '');
         }
     });
 }
